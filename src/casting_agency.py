88b3296a451c94e5bad4e55a164db6b4127c7d2d
Guillaume Payan <payan.guillaume@gmail.com>
2022-09-20 14:46:52 +0200

    removed app.run in casting_agency.py

--- src/casting_agency.py
+++ src/casting_agency.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
 else:
     print('Je comprends pas tout')
     print(__name__)
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    #app.run(host='0.0.0.0', port=8080, debug=True)
